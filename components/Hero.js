eb6055fd33fd379fb3dce905e966f62a68562fc3
devlearningdev <110401395+devlearningdev@users.noreply.github.com>
2022-11-16 11:32:38 +0100

    fixing animation

--- components/Hero.js
+++ components/Hero.js
@@ -11,45 +11,46 @@ import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
-    <motion.div
+    <Flex
+      w={"full"}
+      h={"95vh"}
+      backgroundImage={"../double-helix.png"}
+      backgroundSize={"contain"}
+      backgroundPosition={"left center"}
+      backgroundRepeat="no-repeat"
+      pl="2rem"
+      pr="2rem"
+      _dark={{ bgColor: "black" }}
+    >
+      <Flex w="100%" align="center" justify="center">
+        <Stack maxW="4xl">
+          <Heading
+            as="h1"
+            size="4xl"
+            textShadow="0px 0px 60px"
+            _light={{
+              textShadow: "0px 0px 60px #c987d3",
+            }}
+          >
+            Weird Forms NFT Collection !
+          </Heading>
+
+          <br />
+
+          <Text fontSize="2xl" color="white" _light={{ color: "gray.700" }}>
+            Tempor labore nisi voluptate aute Lorem tempor amet. Ea non elit et
+            tempor minim mollit quis consectetur pariatur elit ut enim non
+            proident.
+          </Text>
+        </Stack>
+      </Flex>
+    </Flex>
+  );
+}
+
+//Animation de venue
+/*<motion.div
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 3 }}
-    >
-      <Flex
-        w={"full"}
-        h={"95vh"}
-        backgroundImage={"../double-helix.png"}
-        backgroundSize={"contain"}
-        backgroundPosition={"left center"}
-        backgroundRepeat="no-repeat"
-        pl="2rem"
-        pr="2rem"
-        _dark={{ bgColor: "black" }}
-      >
-        <Flex w="100%" align="center" justify="center">
-          <Stack maxW="4xl">
-            <Heading
-              as="h1"
-              size="4xl"
-              textShadow="0px 0px 60px"
-              _light={{
-                textShadow: "0px 0px 60px #c987d3",
-              }}
-            >
-              Weird Forms NFT Collection !
-            </Heading>
-
-            <br />
-
-            <Text fontSize="2xl" color="white" _light={{ color: "gray.700" }}>
-              Tempor labore nisi voluptate aute Lorem tempor amet. Ea non elit
-              et tempor minim mollit quis consectetur pariatur elit ut enim non
-              proident.
-            </Text>
-          </Stack>
-        </Flex>
-      </Flex>
-    </motion.div>
-  );
-}
+  ></motion.div>*/
